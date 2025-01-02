f64dd248b6f5c242025307461bcd10a770868db3
한주형 <wngud4950@gmail.com>
2025-01-02 21:48:07 +0900

    refactor: 계산기 클래스 분리

---
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -0,0 +1,41 @@
+package StringCalculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    public static int calculate(String s) {
+        String DEFAULT_DELIMITER = ",|:";
+
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        if (m.find()) {
+            String customDelimiter = Pattern.quote(m.group(1));
+            String numbers = m.group(2);
+            return getTotal(numbers, customDelimiter);
+        }
+
+        return getTotal(s, DEFAULT_DELIMITER);
+    }
+
+    private static int checkMinus(int number) throws IllegalArgumentException{
+        if (number < 0) {
+            throw new IllegalArgumentException("마이너스는 입력할 수 없습니다." + number);
+        }
+        return number;
+    }
+    private static int getTotal(String s, String delimiter) throws IllegalArgumentException {
+        try {
+            return Arrays.stream(s.split(delimiter))
+                    .mapToInt(Integer::parseInt)
+                    .map(StringCalculator::checkMinus)
+                    .sum();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
--- src/test/java/StringCalculator/StringCalculatorTest.java
+++ src/test/java/StringCalculator/StringCalculatorTest.java
@@ -6,96 +6,45 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("StringCalculator 테스트")
 public class StringCalculatorTest {
-    @Test
-    @DisplayName(",로 구분되어 계산 되는 지 확인")
-    public void commaTest() {
-        String s = "1,2,3";
-        assertThat(calculate(s)).isEqualTo(6);
-    }
 
-    @Test
-    @DisplayName(":으로 구분되어 계산 되는 지 확인")
-    public void colonTest() {
-        String s = "1:2:3";
-        assertThat(calculate(s)).isEqualTo(6);
-    }
-
-    @Test
-    @DisplayName(",와 :으로 구분되어 계산 되는 지 확인")
-    public void commaAndColonTest() {
-        String s = "1:2,3";
-        assertThat(calculate(s)).isEqualTo(6);
+    @ParameterizedTest
+    @DisplayName("구분자로 값을 합산")
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1:2,3"})
+    public void calculatesSumForDifferentDelimiters(String input) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(6);
     }
 
     @ParameterizedTest
     @DisplayName("빈값일 경우 0을 반환")
     @NullAndEmptySource
-    public void emptyTest(String s) {
-        assertThat(calculate(s)).isEqualTo(0);
+    public void returnsZeroForEmptyOrNull(String input) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(0);
     }
 
     @ParameterizedTest
-    @DisplayName("한 자리의 문자열일 경우 해당 숫자를 반환")
+    @DisplayName("한 자리 문자열 계산")
     @ValueSource(strings = {"1", "9"})
-    public void digitStringTest(String input) {
-        assertThat(calculate(input)).isEqualTo(Integer.parseInt(input));
+    public void returnsSingleDigitValue(String input) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(Integer.parseInt(input));
     }
 
     @Test
-    @DisplayName("커스텀 구분자가 있을 경우 해당 구분자로 계산")
-    public void customDelimiter() {
+    @DisplayName("커스텀 구분자를 사용한 값 합산")
+    public void calculatesSumForCustomDelimiter() {
         String s = "//;\n1;2;3";
-        assertThat(calculate(s)).isEqualTo(6);
+        assertThat(StringCalculator.calculate(s)).isEqualTo(6);
     }
 
     @Test
-    @DisplayName("음수가 있을 경우 예외가 발생")
-    public void whenMinusGivenExceptionOccur() {
+    @DisplayName("음수가 포함된 경우 예외 발생")
+    public void throwsExceptionForNegativeNumbers() {
         String s = "-1,2:3";
-        assertThatThrownBy(() -> calculate(s)).isInstanceOf(IllegalArgumentException.class);
-    }
-
-
-    private int checkMinus(int number) throws IllegalArgumentException{
-        if (number < 0) {
-            throw new IllegalArgumentException("마이너스는 입력할 수 없습니다.");
-        }
-        return number;
-    }
-    private int getTotal(String s, String delimiter) throws IllegalArgumentException {
-        try {
-            return Arrays.stream(s.split(delimiter))
-                    .mapToInt(Integer::parseInt)
-                    .map(this::checkMinus)
-                    .sum();
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("에러 발생", e);
-        }
-    }
-
-    private int calculate(String s) {
-        String DEFAULT_DELIMITER = ",|:";
-
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
-        if (m.find()) {
-            String customDelimiter = Pattern.quote(m.group(1));
-            String numbers = m.group(2);
-            return getTotal(numbers, customDelimiter);
-        }
-
-        return getTotal(s, DEFAULT_DELIMITER);
+        assertThatThrownBy(() -> StringCalculator.calculate(s)).isInstanceOf(IllegalArgumentException.class);
     }
 }
 
